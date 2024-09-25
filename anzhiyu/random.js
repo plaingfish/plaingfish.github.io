971e1065ef4483c7e86c5ea1c0d59f6c40a95ae0
plaingfish <1965638087@qq.com>
2024-09-25 10:33:29 +0800

    Site updated: 2024-09-25 10:33:28

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/24/Centos7安装Mysql/"];function toRandomPost(){
+var posts=["2024/09/25/Centos7安装Nginx/","2024/09/24/Centos7安装Mysql/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
