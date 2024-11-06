04d961d7c3fb93ec28df84e35724f2ee21e41fe3
plaingfish <1965638087@qq.com>
2024-11-06 22:50:14 +0800

    Site updated: 2024-11-06 22:50:13

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["post/20240925105823.html","post/20240924221104.html","post/20241106204219.html","post/20241106190825.html","post/20240925100320.html"];function toRandomPost(){
+var posts=["post/20240925105823.html","post/20241106190825.html","post/20240924221104.html","post/20240925100320.html","post/20241106210755.html","post/20241106204219.html","post/20241106211540.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
